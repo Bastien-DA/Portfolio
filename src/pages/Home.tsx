6db5e8b8532e0dfac2b2aea842a11f20e6975a17
Bastien-DA <deamezagabastien@outlook.fr>
2024-05-31 12:30:20 +0200

    name and job html

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,12 @@ import {NavBar} from '../components/NavBar.tsx'
 
 function Home() {
     return (
-        <div>
+        <div className="home">
             <NavBar />
+            <div>
+                <h2 className="name">Bastien De Amezaga</h2>
+                <h1 className="job">Développeur étudiant</h1>
+            </div>
         </div>
     )
 }
