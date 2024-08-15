ee2634c39b6b267b7b921d29e1614c830c28e3f4
Logards <deamezagabastien@outlook.fr>
2024-08-15 22:58:56 +0200

    change background color of information section and add code picture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,17 +14,17 @@ function Home() {
             <header>
                 <NavBar />
             </header>
-            <div className="information">
+            <div className="information-section">
                 <section className="profil mr-96">
                     <h2 className="name">Bastien De Amezaga</h2>
-                    <h1 className="job">Développeur junior</h1>
+                    <h1 className="job">DEVELOPPEUR JUNIOR</h1>
                     <button type="button"
                             className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 mt-8"
                             onClick={handleContactClick}>
                         Contact
                     </button>
                 </section>
-                <section className="photo ml-96">
+                <section className="photo ml-85">
                     <img src="/src/assets/photo-de-profil.JPG" alt="Avatar de Bastien De Amezaga" />
                 </section>
                 <div className="arrow">
@@ -33,10 +33,15 @@ function Home() {
                     <span></span>
                 </div>
             </div>
-            <section className="about-me">
-                <h2>A propos</h2>
-                <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Amet magna consectetur rhoncus sagittis faucibus senectus leo. Aliquet sagittis neque et ultrices nascetur morbi venenatis. Ad donec nisl cursus lobortis lacinia ornare. Turpis nisl fermentum interdum efficitur integer parturient sociosqu ullamcorper. Congue sagittis vivamus tincidunt egestas; tristique facilisis molestie dis. Potenti aenean proin ipsum cubilia laoreet quam. Duis pretium a malesuada ornare, ante aptent hac vulputate. Morbi arcu auctor hendrerit cras potenti diam imperdiet! Enim dictum libero senectus iaculis sagittis suspendisse enim et imperdiet.</p>
-            </section>
+            <div className="about-me-section">
+                <section className="code-picture">
+                    <img src="/src/assets/code.jpg" alt="code"/>
+                </section>
+                <section className="about-me">
+                    <h1>A propos</h1>
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Amet magna consectetur rhoncus sagittis faucibus senectus leo. Aliquet sagittis neque et ultrices nascetur morbi venenatis. Ad donec nisl cursus lobortis lacinia ornare. Turpis nisl fermentum interdum efficitur integer parturient sociosqu ullamcorper. Congue sagittis vivamus tincidunt egestas; tristique facilisis molestie dis. Potenti aenean proin ipsum cubilia laoreet quam. Duis pretium a malesuada ornare, ante aptent hac vulputate. Morbi arcu auctor hendrerit cras potenti diam imperdiet! Enim dictum libero senectus iaculis sagittis suspendisse enim et imperdiet.</p>
+                </section>
+            </div>
         </main>
     )
 }
